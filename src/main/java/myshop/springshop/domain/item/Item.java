ef92b35502154954f8bcbf0b2f0ccfa5af3a3e2e
turtle <michael.app.space@gmail.com>
2022-04-22 17:46:18 +0900

    feat: create add and remove stock methods

--- src/main/java/myshop/springshop/domain/item/Item.java
+++ src/main/java/myshop/springshop/domain/item/Item.java
@@ -1,8 +1,8 @@
 package myshop.springshop.domain.item;
 
 import lombok.Getter;
-import lombok.Setter;
 import myshop.springshop.domain.Category;
+import myshop.springshop.exception.NotEnoughStockException;
 
 import javax.persistence.*;
 import java.util.ArrayList;
@@ -12,7 +12,6 @@ import java.util.List;
 @Inheritance(strategy = InheritanceType.SINGLE_TABLE)
 @DiscriminatorColumn(name = "dtype")
 @Getter
-@Setter
 public abstract class Item {
 
     @Id
@@ -25,5 +24,17 @@ public abstract class Item {
     private int stockQuantity;
 
     @ManyToMany(mappedBy = "items")
-    private List<Category> categories = new ArrayList<>();
+    private final List<Category> categories = new ArrayList<>();
+
+    public void addStock(int stockQuantity) {
+        this.stockQuantity += stockQuantity;
+    }
+
+    public void removeStock(int stockQuantity) {
+        int restStock = this.stockQuantity - stockQuantity;
+        if (restStock < 0) {
+            throw new NotEnoughStockException("out of stock");
+        }
+        this.stockQuantity = restStock;
+    }
 }
