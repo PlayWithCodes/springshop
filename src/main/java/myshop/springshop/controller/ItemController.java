c0de34913571310771b066ad8f194d24861b9d53
turtle <michael.app.space@gmail.com>
2022-04-24 10:02:26 +0900

    feat: create item update form and logic

--- src/main/java/myshop/springshop/controller/ItemController.java
+++ src/main/java/myshop/springshop/controller/ItemController.java
@@ -7,6 +7,8 @@ import myshop.springshop.service.ItemService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
@@ -41,5 +43,36 @@ public class ItemController {
         List<Item> items = itemService.findItems();
         model.addAttribute("items", items);
         return "items/itemList";
+    }
+
+    @GetMapping("/items/{itemId}/edit")
+    public String itemUpdateForm(@PathVariable("itemId") Long itemId, Model model) {
+        Book book = (Book) itemService.findItem(itemId);
+
+        BookForm bookForm = new BookForm();
+        bookForm.setId(book.getId());
+        bookForm.setName(book.getName());
+        bookForm.setPrice(book.getPrice());
+        bookForm.setStockQuantity(book.getStockQuantity());
+        bookForm.setAuthor(book.getAuthor());
+        bookForm.setIsbn(book.getIsbn());
+
+        model.addAttribute("form", bookForm);
+        return "items/updateItemForm";
+    }
+
+    @PostMapping("/items/{itemId}/edit")
+    public String itemUpdate(@PathVariable("itemId") Long itemId, @ModelAttribute BookForm bookForm) {
+        Book book = new Book();
+        book.setId(bookForm.getId());
+        book.setName(bookForm.getName());
+        book.setPrice(bookForm.getPrice());
+        book.setStockQuantity(bookForm.getStockQuantity());
+        book.setAuthor(bookForm.getAuthor());
+        book.setIsbn(bookForm.getIsbn());
+
+        itemService.saveItem(book);
+
+        return "redirect:/items";
     }
 }
