1b54a879658982af6109eaf7a625cbf89ca86427
turtle <michael.app.space@gmail.com>
2022-04-24 17:03:58 +0900

    feat: create order form and logic

---
+++ src/main/java/myshop/springshop/controller/OrderController.java
@@ -0,0 +1,43 @@
+package myshop.springshop.controller;
+
+import lombok.RequiredArgsConstructor;
+import myshop.springshop.domain.Member;
+import myshop.springshop.domain.item.Item;
+import myshop.springshop.service.ItemService;
+import myshop.springshop.service.MemberService;
+import myshop.springshop.service.OrderService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor
+public class OrderController {
+
+    private final OrderService orderService;
+    private final MemberService memberService;
+    private final ItemService itemService;
+
+    @GetMapping("/order")
+    public String createForm(Model model) {
+        List<Member> members = memberService.findMembers();
+        List<Item> items = itemService.findItems();
+
+        model.addAttribute("members", members);
+        model.addAttribute("items", items);
+        return "order/orderForm";
+    }
+
+    @PostMapping("/order")
+    public String order(@RequestParam("memberId") Long memberId,
+                        @RequestParam("itemId") Long itemId,
+                        @RequestParam("count") int count) {
+
+        orderService.order(memberId, itemId, count);
+        return "redirect:/orders";
+    }
+}
